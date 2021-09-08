af0d0e7066cc349369148084de6b54357118c594
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2021-09-08 11:45:39 +0200

    Fixed mapping initialization

--- Test/Integration/Controller/RuleBasedMetaTagsTest.php
+++ Test/Integration/Controller/RuleBasedMetaTagsTest.php
@@ -19,15 +19,20 @@ class RuleBasedMetaTagsTest extends \Magento\TestFramework\TestCase\AbstractCont
     /**
      * @magentoDbIsolation enabled
      * @magentoAppIsolation enabled
-     * @magentoDataFixtureBeforeTransaction clearCache
-     * @magentoDataFixtureBeforeTransaction loadRules
+     * @magentoDataFixture Magento/Framework/Search/_files/filterable_attribute.php
+     * @magentoDataFixture loadRules
+     * @magentoDataFixture clearCache
      * @magentoConfigFixture current_store seo/category_metatag_generation/is_enabled 1
      * @dataProvider rulesTestCases
      */
     public function testItUsesRuleSettingWhenCorrectFilterWerePassed($params, $expectedTitle, $expectedDescription)
     {
-        $this->getRequest()->setParams($params);
+        // From ElasticSuite 2.10.6 update Mapping class gets initialized too quickly before filterable attributes
+        // are put into database. This causes missing field mapping errors. We need to remove all shared instances
+        // of all ElasticSuite related classes in order for mapping to get properly regenerated when test is executed
+        $this->removeElasticSuiteClassesInstances();
 
+        $this->getRequest()->setParams($params);
         $this->dispatch('catalog/category/view/id/778');
 
         $response = $this->getResponse()->getBody();
@@ -102,5 +107,19 @@ class RuleBasedMetaTagsTest extends \Magento\TestFramework\TestCase\AbstractCont
         preg_match($headPattern, $html, $results);
 
         return $results[1];
+    }
+
+    protected function removeElasticSuiteClassesInstances() {
+        $reflectionProperty = new \ReflectionProperty(\Magento\TestFramework\ObjectManager::class, '_sharedInstances');
+        $reflectionProperty->setAccessible(true);
+        $sharedInstances = $reflectionProperty->getValue($this->objectManager);
+
+        foreach($sharedInstances as $className => $class) {
+            if(strpos($className, 'Smile') === false) {
+                continue;
+            }
+
+            $this->objectManager->removeSharedInstance($className);
+        }
     }
 }
